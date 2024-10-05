2e075301cfe924435f2bc91de4671a7288b4b849
Tiago <tvhollowknight@gmail.com>
2024-10-04 23:35:12 -0300

    Created the function to play a single game
    With this function a game can be played and the scores will be updated

--- script.js
+++ script.js
@@ -1,26 +1,53 @@
+// initialize scores
 let humanScore = 0;
 let computerScore = 0;
 
+// randomly choose the cpu play
 function getComputerChoice() {
 	let cpuChoice = Math.floor(Math.random() * 100);
 	if (cpuChoice > 66) {
-		return "Rock";
+		return "ROCK";
 	} else if (cpuChoice < 66 && cpuChoice > 33) {
-		return "Paper";
+		return "PAPER";
 	} else {
-		return "Scissors";
+		return "SCISSORS";
 	}
 }
 
+// prompt the player's choice
 function getHumanChoice() {
-	let humanChoice = prompt("What will you play?");
+	let humanChoice = prompt("What will you play?").toUpperCase();
 	if (
-		humanChoice.toUpperCase() != "ROCK" &&
-		humanChoice.toUpperCase() != "PAPER" &&
-		humanChoice.toUpperCase() != "SCISSORS"
+		humanChoice != "ROCK" &&
+		humanChoice != "PAPER" &&
+		humanChoice != "SCISSORS"
 	) {
 		getHumanChoice();
 	} else {
 		return humanChoice;
+	}
+}
+
+function playRound(cpuChoice, humanChoice) {
+	if (cpuChoice == "ROCK" && humanChoice == "SCISSORS") {
+		console.log("You lose! Rock beats scissors!");
+		computerScore += 1;
+	} else if (cpuChoice == "PAPER" && humanChoice == "ROCK") {
+		console.log("You lose! PAPER beats rock!");
+		computerScore += 1;
+	} else if (cpuChoice == "SCISSORS" && humanChoice == "PAPER") {
+		console.log("You lose! SCISSORS beats paper!");
+		computerScore += 1;
+	} else if (humanChoice == "ROCK" && cpuChoice == "SCISSORS") {
+		console.log("You Win! Rock beats scissors");
+		humanScore += 1;
+	} else if (humanChoice == "PAPER" && cpuChoice == "SCISSORS") {
+		console.log("You Win! Paper beats scissors");
+		humanScore += 1;
+	} else if (humanChoice == "SCISSORS" && cpuChoice == "PAPER") {
+		console.log("You Win! Scissors beats paper");
+		humanScore += 1;
+	} else {
+		console.log("That's a tie!");
 	}
 }
