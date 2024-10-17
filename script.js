5e1c4802b8e73c8e61e77f6233c2b8826539b03f
Tiago <tvhollowknight@gmail.com>
2024-10-17 14:24:26 -0300

    Finished the UI of rps
    Users can now play the game on the UI instead of the command line

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
-// randomly choose the cpu play
+// create event listeners for each button
+const divChoices = document.querySelector(".choices");
+divChoices.addEventListener("click", (e) => {
+	let target = e.target;
+
+	switch (target.id) {
+		case "rock":
+			playRound(getComputerChoice(), "ROCK");
+			break;
+		case "paper":
+			playRound(getComputerChoice(), "PAPER");
+			break;
+		case "scissors":
+			playRound(getComputerChoice(), "SCISSORS");
+			break;
+	}
+});
+
+// initialize scores
+let humanScore = 0;
+let computerScore = 0;
+
+// randomly chooses the cpu choice
 function getComputerChoice() {
 	let cpuChoice = Math.floor(Math.random() * 3);
 	if (cpuChoice == 0) {
@@ -10,68 +32,75 @@ function getComputerChoice() {
 	}
 }
 
-// prompt the player's choice
-function getHumanChoice() {
-	let humanChoice = prompt("What will you play?").toUpperCase();
-	if (
-		humanChoice != "ROCK" &&
-		humanChoice != "PAPER" &&
-		humanChoice != "SCISSORS"
-	) {
-		getHumanChoice();
+// plays a round of rock paper scissors and adds to the winner score
+function playRound(cpuChoice, humanChoice) {
+	// erase previous winner display
+	let winnerDisplay = document.querySelector(".winner");
+	if (winnerDisplay.firstChild != null) {
+		winnerDisplay.removeChild(winnerDisplay.firstChild);
+	}
+
+	// get the p that displays the score
+	let cpuScoreDisplay = document.querySelector(".computerScore");
+	let humanScoreDisplay = document.querySelector(".humanScore");
+
+	humanScoreDisplay.textContent = `Your Score = ${humanScore}`;
+	cpuScoreDisplay.textContent = `Computer Score = ${computerScore}`;
+
+	// check the winner of that round, update the scores and the page
+	if (cpuChoice == "ROCK" && humanChoice == "SCISSORS") {
+		alert("You lose! Rock beats scissors!");
+		computerScore += 1;
+		cpuScoreDisplay.textContent = `Computer Score = ${computerScore}`;
+	} else if (cpuChoice == "PAPER" && humanChoice == "ROCK") {
+		alert("You lose! Paper beats rock!");
+		computerScore += 1;
+		cpuScoreDisplay.textContent = `Computer Score = ${computerScore}`;
+	} else if (cpuChoice == "SCISSORS" && humanChoice == "PAPER") {
+		alert("You lose! Scissor beats paper!");
+		computerScore += 1;
+		cpuScoreDisplay.textContent = `Computer Score = ${computerScore}`;
+	} else if (humanChoice == "ROCK" && cpuChoice == "SCISSORS") {
+		alert("You Win! Rock beats scissors");
+		humanScore += 1;
+		humanScoreDisplay.textContent = `Your Score = ${humanScore}`;
+	} else if (humanChoice == "PAPER" && cpuChoice == "SCISSORS") {
+		alert("You Win! Paper beats scissors");
+		humanScore += 1;
+		humanScoreDisplay.textContent = `Your Score = ${humanScore}`;
+	} else if (humanChoice == "SCISSORS" && cpuChoice == "PAPER") {
+		alert("You Win! Scissors beats paper");
+		humanScore += 1;
+		humanScoreDisplay.textContent = `Your Score = ${humanScore}`;
 	} else {
-		return humanChoice;
+		alert("That's a tie!");
+	}
+
+	// check if someone won and display the winner
+	if (humanScore >= 5 || computerScore >= 5) {
+		displayWinner();
 	}
 }
 
-function playGame() {
-	// initialize scores
-	let humanScore = 0;
-	let computerScore = 0;
+// display the winner of a number of rounds
+function displayWinner() {
+	let roundWinnerDisplay = document.querySelector(".winner");
+	let winner = document.createElement("h1");
 
-	// play a round of rock paper scissors and add to the winner score
-	function playRound(cpuChoice, humanChoice) {
-		if (cpuChoice == "ROCK" && humanChoice == "SCISSORS") {
-			alert("You lose! Rock beats scissors!");
-			computerScore += 1;
-		} else if (cpuChoice == "PAPER" && humanChoice == "ROCK") {
-			alert("You lose! Paper beats rock!");
-			computerScore += 1;
-		} else if (cpuChoice == "SCISSORS" && humanChoice == "PAPER") {
-			alert("You lose! Scissor beats paper!");
-			computerScore += 1;
-		} else if (humanChoice == "ROCK" && cpuChoice == "SCISSORS") {
-			alert("You Win! Rock beats scissors");
-			humanScore += 1;
-		} else if (humanChoice == "PAPER" && cpuChoice == "SCISSORS") {
-			alert("You Win! Paper beats scissors");
-			humanScore += 1;
-		} else if (humanChoice == "SCISSORS" && cpuChoice == "PAPER") {
-			alert("You Win! Scissors beats paper");
-			humanScore += 1;
-		} else {
-			alert("That's a tie!");
-		}
-	}
-	for (i = 0; i < 5; i++) {
-		// get the user and cpu selections to play the game 5 times
-		const humanSelection = getHumanChoice();
-		const computerSelection = getComputerChoice();
-		playRound(computerSelection, humanSelection);
-	}
 	if (humanScore > computerScore) {
-		alert(
-			`Your Score: ${humanScore}\nComputer Score: ${computerScore}\nYou Won!!!`
-		);
+		winner.textContent = "YOU WIN!!!";
+		roundWinnerDisplay.appendChild(winner);
+		humanScore = 0;
+		computerScore = 0;
 	} else if (humanScore < computerScore) {
-		alert(
-			`Your Score: ${humanScore}\nComputer Score: ${computerScore}\nYou Lost.`
-		);
+		winner.textContent = "YOU LOSE.";
+		roundWinnerDisplay.appendChild(winner);
+		humanScore = 0;
+		computerScore = 0;
 	} else {
-		alert(
-			`Your Score: ${humanScore}\nComputer Score: ${computerScore}\nIt's a Tie!`
-		);
+		winner.textContent = "IT'S A TIE!";
+		roundWinnerDisplay.appendChild(winner);
+		humanScore = 0;
+		computerScore = 0;
 	}
 }
-
-playGame();
